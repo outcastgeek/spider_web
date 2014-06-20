1a95bf8adbb0b549781d438a96d046a8643adb6c
outcastgeek <outcastgeek+git@gmail.com>
2014-06-20 10:07:45 -0500

    Adding ability to collect urls from fetched page

--- crawlers/async_crawler/src/async_crawler/toile/spider.clj
+++ crawlers/async_crawler/src/async_crawler/toile/spider.clj
@@ -9,11 +9,34 @@
   (let [response (client/get url {:as :stream})
         {headers :headers
          body :body
-         status :status} response]
+         status :status} response
+        host (cond
+               (.endsWith url "/") url
+               :else url)]
     (debug "Fetched: " url)
-    {:headers headers
+    {:url host
+     :headers headers
      :body (html/html-resource body)
      :status status}))
+
+(defn urls [pdata]
+  (let [url (:url pdata)
+        hrefs (pmap
+                #(-> % :attrs :href)
+                (html/select (:body pdata) #{[:a]}))
+        not-nil-hrefs (remove nil? hrefs)
+        no-frag-hrefs (remove #(.startsWith % "#") not-nil-hrefs)
+        urls (->> no-frag-hrefs
+                  (pmap #(cond
+                          (.startsWith % "/") (str url %)
+                          :else %))
+                  (pmap #(cond
+                          (not
+                            (.startsWith % "http")) (str url "/" %)
+                          :else %)))]
+    (debug (format "Retrieved %d urls from %s" (count urls) url))
+    urls))
+
 
 
 
