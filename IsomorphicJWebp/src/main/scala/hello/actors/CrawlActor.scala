df9c54b8c6e8da7ec7258d46f8dd2580d67b3baf
outcastgeek <outcastgeek+git@gmail.com>
2015-04-15 05:35:12 -0500

    Akka Jsoup Crawler (in progress)

--- IsomorphicJWebp/src/main/scala/hello/actors/CrawlActor.scala
+++ IsomorphicJWebp/src/main/scala/hello/actors/CrawlActor.scala
@@ -4,10 +4,12 @@ import akka.actor.{Actor, ActorRef, Cancellable}
 import akka.event.Logging
 import hello.actors.Messages.{Get, NoReply, Reply}
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 
+import scala.collection.immutable.Map
 import scala.concurrent.duration._
 
 /**
@@ -23,25 +25,38 @@ class CrawlActor @Autowired() (actorFactory: ActorFactory) extends Actor {
 
   val restClient = actorFactory.genRestClientActor
 
+  val URL_REGEX = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]".r
+
   override def receive = {
     case Get(ip_or_hostname) =>
       val url = s"http://$ip_or_hostname"
       restClient ! Get(url)
       implicit val ec = context.dispatcher
       val timeout = context.system.scheduler.scheduleOnce(2 seconds, self, NoReply(sender))
-      context become extractLinks(sender, timeout)
+      context become extractLinks(sender, url, timeout)
     case _ =>
       log.info("Received Unknown Message")
   }
 
-  def extractLinks(actorRef: ActorRef, timeout: Cancellable): Receive = {
+  def extractLinks(origin: ActorRef, url: String, timeout: Cancellable): Receive = {
     case Reply(rawPageData) =>
       timeout.cancel()
       val pageData = Jsoup.parse(rawPageData.getOrElse(RestClientActor.replyKey, "").asInstanceOf[String])
-      val pageLinks = pageData.select("a[href]").toArray().toList
-      pageLinks foreach { link =>
-        log.info(link.toString)
+      val pageLinks = pageData.select("a[href]").toArray().toList.asInstanceOf[List[Element]]
+      val collectedURLS = pageLinks map { link =>
+        val href = link.attr("href")
+        href match {
+          case URL_REGEX(href) =>
+            val wfURL = href
+            log.info(wfURL)
+            wfURL
+          case _ =>
+            val pURL = s"$url$href"
+            log.info(pURL)
+            pURL
+        }
       }
+      origin ! Reply(Map(CrawlActor.replyKey -> collectedURLS))
       context become receive
     case NoReply(origin) =>
       log.info(errMsg)
